49a0b07583a070922e33fe4c3dfd25c4048c4908
Hernan Foffani <hfoffani@gmail.com>
2015-11-12 14:07:03 +0000

    Exit program.

--- LLconsole/Program.cs
+++ LLconsole/Program.cs
@@ -10,8 +10,7 @@ namespace LLconsole {
                 var exp = new LambdaLang.Expression();
                 exp.SetExpression(program);
                 var res = exp.Solve();
-                Console.WriteLine();
-                Console.WriteLine(res.ToString());
+                Console.WriteLine(">>> " + res.ToString());
                 if (exp.LastError != "") {
                     Console.WriteLine(exp.LastError);
                 }
@@ -23,17 +22,19 @@ namespace LLconsole {
 
         static void loop() {
             string l;
+            Console.WriteLine("... Enter a LambdaLang expression or program.");
+            Console.WriteLine("... ENTER to execute. ^Z ENTER to exit.");
             do {
-                Console.WriteLine("... Enter a LambdaLang expression or program.");
-                Console.WriteLine("... ENTER ENTER to run. ^Z ENTER to exit.");
+                Console.WriteLine("");
                 var pg = "";
                 do {
                     l = Console.ReadLine();
                     if (l != null)
                         pg += l;
                 } while (l != null && l != "");
-                if (!string.IsNullOrEmpty(pg))
-                    run(pg);
+                if (string.IsNullOrEmpty(pg))
+                    return;
+                run(pg);
             } while (l != null);
         }
 
